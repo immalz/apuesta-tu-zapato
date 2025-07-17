db0db90b79101b88991819fd9d1f4dfda12efe91
Marco Alzamora Cueto <alzamora1008@gmail.com>
2025-07-16 22:19:31 -0500

    agregando mejoras al card

--- src/app/public/layout/layout.component.ts
+++ src/app/public/layout/layout.component.ts
@@ -13,4 +13,55 @@ import { FooterComponent } from '../../core/components/footer/footer.component';
 })
 export class LayoutComponent {
 
+  matchs: any[] = [
+    {
+      deck1: {
+        id: 1,
+        name: 'Sakuyamon',
+        description: 'This is the first deck.',
+        player: 'Player 1',
+        image: 'assets/images/decks/sakuyamon.webp',
+      },
+      deck2: {
+        id: 2,
+        name: 'Kaguyamon',
+        description: 'This is the second deck.',
+        player: 'Player 1',
+        image: 'assets/images/decks/galaxy.jpeg',
+      }
+    },
+    {
+      deck1: {
+        id: 3,
+        name: 'Royal Knight',
+        description: 'This is the third deck.',
+        player: 'Player 1',
+        image: 'assets/images/decks/rk.jpg',
+      },
+      deck2: {
+        id: 4,
+        name: 'Gallantmon',
+        description: 'This is the fourth deck.',
+        player: 'Player 1',
+        image: 'assets/images/decks/alters.jpg',
+      }
+    },
+    {
+      deck1: {
+        id: 5,
+        name: 'Phoenixmon',
+        description: 'This is the fifth deck.',
+        player: 'Player 1',
+        image: 'assets/images/decks/pollos.jpg',
+      },
+      deck2: {
+        id: 6,
+        name: 'Seven Lords',
+        description: 'This is the sixth deck.',
+        player: 'Player 1',
+        image: 'assets/images/decks/sevenlord.jpg',
+      }
+    }
+  ];
+  
 }
